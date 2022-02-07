ccb37e0daf575b96fac0674086dba849152b5f62
Sevrinn <sevrinnsevrinn@gmail.com>
2022-02-07 08:07:37 -0800

    programmed sort interface on sortBy toggle dropdown

--- src/App.js
+++ src/App.js
@@ -8,14 +8,23 @@ import AppointmentInfo from './components/AppointmentInfo'
 function App() {
   let [appointmentList, setAppoinmentList] = useState([])
   let [query, setQuery] = useState('')
+  let [sortBy, setSortBy] = useState('petName')
+  let [orderBy, setOrderBy] = useState('asc')
 
-  const filteredAppointments = appointmentList.filter((item) => {
-    return (
-      item.petName.toLowerCase().includes(query.toLowerCase()) ||
-      item.ownerName.toLowerCase().includes(query.toLowerCase()) ||
-      item.aptNotes.toLowerCase().includes(query.toLowerCase())
-    )
-  })
+  const filteredAppointments = appointmentList
+    .filter((item) => {
+      return (
+        item.petName.toLowerCase().includes(query.toLowerCase()) ||
+        item.ownerName.toLowerCase().includes(query.toLowerCase()) ||
+        item.aptNotes.toLowerCase().includes(query.toLowerCase())
+      )
+    })
+    .sort((a, b) => {
+      let order = orderBy === 'asc' ? 1 : -1
+      return a[sortBy].toLowerCase() < b[sortBy].toLowerCase()
+        ? -1 * order
+        : 1 * order
+    })
 
   const fetchData = useCallback(() => {
     fetch('./data.json')
@@ -35,7 +44,14 @@ function App() {
         Appointments
       </h1>
       <AddAppointment />
-      <Search query={query} onQueryChange={(myQuery) => setQuery(myQuery)} />
+      <Search
+        query={query}
+        onQueryChange={(myQuery) => setQuery(myQuery)}
+        orderBy={orderBy}
+        onOrderByChange={(mySort) => setOrderBy(mySort)}
+        sortBy={sortBy}
+        onSortByChange={(mySort) => setSortBy(mySort)}
+      />
 
       <ul className='divide-y divide-gray-200'>
         {filteredAppointments.map((appointment) => (
